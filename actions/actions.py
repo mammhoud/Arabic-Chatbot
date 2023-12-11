6288f08b0858439bb718e9a87383693c32d08ac2
Mahmoud Ezzat <mahmoud.ezat@outlook.com>
2023-12-12 01:53:35 +0200

    'new changes'

--- actions/actions.py
+++ actions/actions.py
@@ -14,30 +14,26 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import requests
+from rasa_sdk.events import SlotSet
+from rasa_sdk.forms import FormValidationAction
+
+class SubmitReviewForm(FormValidationAction):
+    def name(self) -> str:
+        return "submit_review_form"
+
+    async def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: "DomainDict") -> list:
+        name = tracker.get_slot("name")
+        rating = tracker.get_slot("rating")
+        review = tracker.get_slot("review")
+
+        if name is not None and rating is not None and review is not None:
+            review_text = f"Name: {name}, Rating: {rating}, Review: {review}"
+            return [SlotSet("name", None), SlotSet("rating", None), SlotSet("review", None), SlotSet("user_reviews", review_text)]
+        else:
+            dispatcher.utter_message(text="عفوا لوجود بعض الاخطاء الان يمكنك ارسال رايك فى اي وقت و سيتم التجاوب معك منا!.")
+            return []
 
 
-
-# class ActionCheckLang(Action):
-    # def name(self) -> Text:
-        # return "action_check_lang"
-    # def run(self, dispatcher: CollectingDispatcher,
-            # tracker: Tracker,
-            # domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-            
-            
-        # # to-Do : create which language api
-        # selected_lag = next(tracker.get_latest_entity_values("selected_lag"),None)
-        # url = "https://api.apilayer.com/exchangerates_data/"
-        
-        # # change config files nlu language with the selected_lag if lang is ar or en
-        # # this action for user language or unknown language to the bot
-        # # translate the user input to the common file language if the user input is not arabic or english
-        # # first check the user input language, if the language is = ar, en make the nlu file path= the {language_folder} path
-        # # then if the language is not handled into nlu file make translation api cached to make new language translation with the nlu default input
-        # # considering the best fitting language to train the models (arabic, english) with our fanctions
-        # # create new data base with languages
-      
-        # dispatcher.utter_message(text= f"selected_lag = "{selected_lag}")
 class ActionBankingCardActivated(Action):
     def name(self) -> Text:
         return "action_banking_card_activated"
@@ -47,7 +43,7 @@ class ActionBankingCardActivated(Action):
         card_number = next(tracker.get_latest_entity_values("card_number"), None)
         #pattern = re.compile("^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$")
         #if pattern.match(card_number):
-        dispatcher.utter_message(text="تم تفعيل البطاقة")
+        dispatcher.utter_message(text="تم التاكد من اشتراكك شكرا لك على ثقتكم")
         
         #else:
         #    dispatcher.utter_message(text="Invalid number!")
@@ -65,21 +61,21 @@ class ActionBankingCardActivated(Action):
         # dispatcher.utter_message(
             # text="Hello!", to=user_id
         # )
-        # return []
-class ActionBankingExchangeRate(Action):
-    def name(self) -> Text:
-        return "action_exchange_rate"
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        url = "https://api.apilayer.com/exchangerates_data/"
+#         # return []
+# class ActionBankingExchangeRate(Action):
+#     def name(self) -> Text:
+#         return "action_exchange_rate"
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         url = "https://api.apilayer.com/exchangerates_data/"
 
-        payload = {}
-        headers= {
-        "apikey": "NKd5K7kfckOVRG3KdMWNk8Q1hj0E7bID"
-        }
-        response = requests.request("GET", url, headers=headers, data = payload)
-        responseJSON = response.json();
-        status_code = response.status_code
-        dispatcher.utter_message(text=str(responseJSON['result']));
-        return []
+#         payload = {}
+#         headers= {
+#         "apikey": "NKd5K7kfckOVRG3KdMWNk8Q1hj0E7bID"
+#         }
+#         response = requests.request("GET", url, headers=headers, data = payload)
+#         responseJSON = response.json();
+#         status_code = response.status_code
+#         dispatcher.utter_message(text=str(responseJSON['result']));
+#         return []
